4d65131bdd2f501b8ac39d3e6bf7c3f28ee1d29e
daniel endaylalu <endaylaludaniel@gmail.com>
2024-07-11 11:39:42 +0300

    added fav icons

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -26,6 +26,26 @@ export default async function RootLayout({ children, params: { locale } }) {
   const { resources } = await initTranslations(locale, i18nNamespaces);
   return (
     <html lang={locale} dir={dir(locale)}>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${inter.className} flex flex-col h-[100vh] justify-between`}
       >
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
 import React from 'react';
 import LanguageChanger from './LanguageChanger';
+import Image from 'next/image';
+
+import logo from '../assets/logo2.png';
 
 const Navbar = () => {
   return (
     <div className="w-full bg-inherit fixed z-20 top-0 left-0 flex flex-wrap gap-4 sm:flex-nowrap justify-between items-center py-4 px-8 sm:px-16">
-      <div className="logo">
-        <Link href="/" className="font-bold text-2xl">
-          divinesmm
+      <div className="logo  flex gap-x-4 items-center">
+        <Link href="/">
+          <Image src={logo} className="w-[60px] h-[60px]" alt="logo" />
         </Link>
-        <LanguageChanger />
+
+        <div>
+          <Link href="/" className="font-bold text-2xl">
+            divinesmm
+          </Link>
+          <LanguageChanger />
+        </div>
       </div>
       <div className="flex items-center sm:gap-x-8 gap-x-6">
         <Link className=" text-sm sm:texbase" href="#services">
